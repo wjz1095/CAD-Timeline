4d2906a961e10364fda5eaf02ecf69a7a07d5350
Wally <wjz1095@gmail.com>
2019-08-18 16:20:13 -0400

    Adding all files

---
+++ RTFC-CAD V1/RTFC-CAD/MainForm.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.InputTextBox = New System.Windows.Forms.TextBox()
+        Me.DisplayTextBox = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.CurrentTimeLabel = New System.Windows.Forms.Label()
+        Me.CurrentDateLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'InputTextBox
+        '
+        Me.InputTextBox.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.InputTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.InputTextBox.Location = New System.Drawing.Point(0, 557)
+        Me.InputTextBox.Name = "InputTextBox"
+        Me.InputTextBox.Size = New System.Drawing.Size(854, 20)
+        Me.InputTextBox.TabIndex = 1
+        '
+        'DisplayTextBox
+        '
+        Me.DisplayTextBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DisplayTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplayTextBox.Location = New System.Drawing.Point(-12, -2)
+        Me.DisplayTextBox.Multiline = True
+        Me.DisplayTextBox.Name = "DisplayTextBox"
+        Me.DisplayTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.DisplayTextBox.Size = New System.Drawing.Size(866, 553)
+        Me.DisplayTextBox.TabIndex = 2
+        Me.DisplayTextBox.TabStop = False
+        '
+        'Timer1
+        '
+        '
+        'CurrentTimeLabel
+        '
+        Me.CurrentTimeLabel.AutoSize = True
+        Me.CurrentTimeLabel.BackColor = System.Drawing.Color.LightCoral
+        Me.CurrentTimeLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CurrentTimeLabel.Location = New System.Drawing.Point(12, 9)
+        Me.CurrentTimeLabel.Name = "CurrentTimeLabel"
+        Me.CurrentTimeLabel.Size = New System.Drawing.Size(104, 25)
+        Me.CurrentTimeLabel.TabIndex = 2
+        Me.CurrentTimeLabel.Text = "00:00:00"
+        '
+        'CurrentDateLabel
+        '
+        Me.CurrentDateLabel.AutoSize = True
+        Me.CurrentDateLabel.BackColor = System.Drawing.Color.LightCoral
+        Me.CurrentDateLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CurrentDateLabel.Location = New System.Drawing.Point(186, 9)
+        Me.CurrentDateLabel.Name = "CurrentDateLabel"
+        Me.CurrentDateLabel.Size = New System.Drawing.Size(163, 25)
+        Me.CurrentDateLabel.TabIndex = 3
+        Me.CurrentDateLabel.Text = "March 2, 2018"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(854, 577)
+        Me.Controls.Add(Me.InputTextBox)
+        Me.Controls.Add(Me.CurrentDateLabel)
+        Me.Controls.Add(Me.CurrentTimeLabel)
+        Me.Controls.Add(Me.DisplayTextBox)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimumSize = New System.Drawing.Size(870, 616)
+        Me.Name = "MainForm"
+        Me.Text = "Timestamper"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents InputTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents DisplayTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents CurrentTimeLabel As System.Windows.Forms.Label
+    Friend WithEvents CurrentDateLabel As System.Windows.Forms.Label
+
+End Class
---
+++ RTFC-CAD V1/RTFC-CAD/MainForm.vb
@@ -0,0 +1,54 @@
+﻿Public Class MainForm
+
+
+    Private Sub MainForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Start() ' Start the timer
+        CurrentDateLabel.Text = System.DateTime.Now.ToString("dddd, MMMM dd yyyy") ' Show the date on the CurrentDateLabel on launch
+
+        'When program launches, create file and insert BEGIN OF FILE string
+        Dim appPath As String = Application.StartupPath()
+        appPath = appPath + "\STAMP-ENTRY " + System.DateTime.Now.ToString("MM-dd-yyyy") + ".txt"
+        My.Computer.FileSystem.WriteAllText(appPath, Environment.NewLine + "File created at: " + appPath, True)
+        My.Computer.FileSystem.WriteAllText(appPath, Environment.NewLine + "------BEGIN OF FILE------ Time: " + TimeOfDay.ToString("HH:mm:ss") + "EST", True)
+        DisplayTextBox.Text = "File created at: " + appPath
+
+        Me.Show()
+        MessageBox.Show("The Timestamper program will automatically create a .txt file in the directory: " + appPath + " The file will be named '" + "STAMP-ENTRY " + System.DateTime.Now.ToString("MM-dd-yyyy") + ".txt'", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        ' When the timer ticks, it updates the time display on the form, and the date
+        CurrentTimeLabel.Text = TimeOfDay.ToString("HH:mm:ss") + "EST"
+        CurrentDateLabel.Text = System.DateTime.Now.ToString("dddd, MMMM dd yyyy") ' This is here so if the program is open while the time ticks over to midnight, it will change the date as well
+    End Sub
+
+    ' When Enter is pressed, it will write the date and time along with the contents of the input text box to the display text box
+    Private Sub InputTextBox_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles InputTextBox.KeyDown
+        Dim OutputString As String = ""
+        If e.KeyCode = Keys.Enter Then
+            If InputTextBox.Text <> "" Then ' If the text box is NOT empty then it will continue with appending to the text box
+                OutputString = System.DateTime.Now.ToString("MM/dd/yyyy ") + TimeOfDay.ToString("HH:mm:ss") + " EST   |   " + InputTextBox.Text
+
+                OutputString = OutputString.ToUpper()
+                DisplayTextBox.AppendText(Environment.NewLine + OutputString)
+
+                Dim appPath As String = Application.StartupPath()
+                appPath = appPath + "\STAMP-ENTRY " + System.DateTime.Now.ToString("MM-dd-yyyy") + ".txt"
+                My.Computer.FileSystem.WriteAllText(appPath, Environment.NewLine + OutputString, True)
+                InputTextBox.Text = ""
+            End If
+        End If
+    End Sub
+
+    ' When attempting to close the form, it will ask and confirm if you want to close it instead of just closing
+    Private Sub MainForm_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        If MessageBox.Show("Are you sure you want to close the timestamper?", "Timestamper Close Confirmation", MessageBoxButtons.YesNo) = DialogResult.No Then
+            ' Cancel the Closing event from closing the form.
+            e.Cancel = True
+        Else
+            Dim appPath As String = Application.StartupPath()
+            appPath = appPath + "\STAMP-ENTRY " + System.DateTime.Now.ToString("MM-dd-yyyy") + ".txt"
+            My.Computer.FileSystem.WriteAllText(appPath, Environment.NewLine + "------END  OF  FILE------ Time:" + TimeOfDay.ToString("HH:mm:ss") + "EST", True)
+        End If
+    End Sub
+End Class
